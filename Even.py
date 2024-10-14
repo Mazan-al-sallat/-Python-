ea7bb03418e32c65fbfee056d7f18722019e7ea7
Mazan-al-sallat <mazenalsallat@gmail.com>
2024-10-14 22:57:44 +0300

    Add files via upload

---
+++ Even.py
@@ -0,0 +1,9 @@
+def print_even_numbers(n):
+    print("Even numbers from 1 to", n, "are:")
+    for num in range(1, n + 1):
+        if num % 2 == 0:
+            print(num)
+
+# Example usage
+n = 20
+print_even_numbers(n)
